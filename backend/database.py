1ee1e3a64006d427136bfdd4d47561a3a47256a2
Bruno Amaral Teixeira de Freitas <brunoatf257@gmail.com>
2024-06-28 01:26:34 -0300

    added precommit and poetry

--- backend/database.py
+++ backend/database.py
@@ -1,31 +1,35 @@
-from datetime import date
 import json
-from pydantic import BaseModel
-
 import typing
+from datetime import date
+
 
 class LawsuitDatabase:
-
     """
     This class encapsulates the lawsuit database and provides methods to search for lawsuits.
 
     Considerations:
 
-    - I implemented the database as a simple .json file for the sake of simplicity, as recommended by the challenge rules. 
+    - I implemented the database as a .json file for the sake of simplicity, as recommended by the challenge rules.
     - In a real-world scenario, this class would interact with a real database such as Redis.
     - Finally, I used static methods as the class does not need to maintain any state or have multiple instances.
     """
 
     @staticmethod
     def _load_lawsuits() -> dict:
-        with open('data/lawsuits.json') as f:
+        with open("data/lawsuits.json") as f:
             return json.load(f)
-    
+
     @staticmethod
-    def search_lawsuits(cnj: str = None, court: str = None, start_date_interval: date = None, end_date_interval: date = None, plaintiff: str = None, defendant: str = None) -> typing.List[dict]:
-        
+    def search_lawsuits(
+        cnj: str = None,
+        court: str = None,
+        start_date_interval: date = None,
+        end_date_interval: date = None,
+        plaintiff: str = None,
+        defendant: str = None,
+    ) -> typing.List[dict]:
         """Performs a search in the lawsuit database based on the provided parameters.
-        
+
         Args:
             cnj (str): The CNJ (National Justice Council) number of the lawsuit in the format NNNNNNN-NN.NNNN.N.NN.NNNN
             court (str): The court where the lawsuit is being processed.
@@ -42,20 +46,18 @@ class LawsuitDatabase:
         results = []
 
         for lawsuit in lawsuits:
-            if cnj and lawsuit['cnj'] != cnj:
+            if cnj and lawsuit["cnj"] != cnj:
                 continue
-            if court and lawsuit['court'] != court:
+            if court and lawsuit["court"] != court:
                 continue
-            if start_date_interval and lawsuit['start_date'] < start_date_interval:
+            if start_date_interval and lawsuit["start_date"] < start_date_interval:
                 continue
-            if end_date_interval and lawsuit['start_date'] > end_date_interval:
+            if end_date_interval and lawsuit["start_date"] > end_date_interval:
                 continue
-            if plaintiff and lawsuit['plaintiff'] != plaintiff:
+            if plaintiff and lawsuit["plaintiff"] != plaintiff:
                 continue
-            if defendant and lawsuit['defendant'] != defendant:
+            if defendant and lawsuit["defendant"] != defendant:
                 continue
             results.append(lawsuit)
 
         return results
-
-
--- backend/schemas.py
+++ backend/schemas.py
@@ -1,13 +1,15 @@
-from typing import List
 from datetime import date
-import strawberry
+from typing import List
 
+import strawberry
 from database import LawsuitDatabase
+
 
 @strawberry.type
 class Movement:
     movement_date: date
     description: str
+
 
 @strawberry.type
 class Lawsuit:
@@ -18,13 +20,12 @@ class Lawsuit:
     defendant: str
     movements: List[Movement]
 
+
 @strawberry.type
 class Query:
-
     def _format_results(results: dict) -> List[Lawsuit]:
-
         """Formats the search results into a list of Lawsuit objects.
-        
+
         Args:
 
             results (dict): The search results from the database.
@@ -34,18 +35,38 @@ class Query:
             A list of Lawsuit objects.
         """
 
-        return [Lawsuit(
-            cnj=result['cnj'],
-            court=result['court'],
-            start_date=result['start_date'],
-            plaintiff=result['plaintiff'],
-            defendant=result['defendant'],
-            movements=[Movement(movement_date=movement['movement_date'], description=movement['description']) for movement in result['movements']]
-        ) for result in results]
+        return [
+            Lawsuit(
+                cnj=result["cnj"],
+                court=result["court"],
+                start_date=result["start_date"],
+                plaintiff=result["plaintiff"],
+                defendant=result["defendant"],
+                movements=[
+                    Movement(
+                        movement_date=movement["movement_date"],
+                        description=movement["description"],
+                    )
+                    for movement in result["movements"]
+                ],
+            )
+            for result in results
+        ]
 
     @strawberry.field
-    def lawsuit(self, cnj: str = None, court: str = None, start_date_interval: date = None, end_date_interval: date = None, plaintiff: str = None, defendant: str = None) -> List[Lawsuit]:
-        results = LawsuitDatabase.search_lawsuits(cnj, court, start_date_interval, end_date_interval, plaintiff, defendant)
+    def lawsuit(
+        self,
+        cnj: str = None,
+        court: str = None,
+        start_date_interval: date = None,
+        end_date_interval: date = None,
+        plaintiff: str = None,
+        defendant: str = None,
+    ) -> List[Lawsuit]:
+        results = LawsuitDatabase.search_lawsuits(
+            cnj, court, start_date_interval, end_date_interval, plaintiff, defendant
+        )
         return Query._format_results(results)
+
 
 schema = strawberry.Schema(query=Query)
